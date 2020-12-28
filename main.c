8d5197f3dfdd5a0cfcecb5d7984041a3be830163
npyl <ulcheats@gmail.com>
2020-12-28 19:44:30 +0200

    semaphore logic

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/mman.h>   // shm_open, mmap, munmap
+#include <semaphore.h>
 
 #define MIN_CHILDREN 1
 #define MAX_CHILDREN 1000
+
+// shared
+static int *p = NULL;
 
 void critical_region(int i)
 {
@@ -38,9 +42,18 @@ int get_number_of_children(int argc, char** argv)
 
 int main(int argc, char** argv)
 {
-    int N           = get_number_of_children(argc, argv);
-    pid_t* pid      = calloc(N, sizeof(pid_t));
-    int *p          = NULL;
+    int N       = get_number_of_children(argc, argv);
+    pid_t* pid  = calloc(N, sizeof(pid_t));
+    sem_t* sem  = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    int child_status;
+
+    // create a shared-between-processes semaphore (PSHARED must be 1)
+    // Initialise to 1 so that all forks race to get it (probably the first one to be created will get it though)
+    sem_init(sem, 1, 1);
+
+    // make p shared
+    p = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
     /*
      * Create N children.
@@ -55,7 +68,9 @@ int main(int argc, char** argv)
              * As a child, I will try to access my critical region
              * and then I will terminate normally using _exit(0).
              */
+            sem_wait(sem);
             critical_region(i);
+            sem_post(sem);
             _exit(0);
         }
     }
@@ -82,6 +97,12 @@ int main(int argc, char** argv)
 
     // Once every child has exited, print p
     printf("p = %d\n", *p);
+
+    // sem_destroy or munmap??
+    sem_destroy(sem);
+
+    // deallocate shared p
+    munmap(p, sizeof(int));
 
     return 0;
 }
